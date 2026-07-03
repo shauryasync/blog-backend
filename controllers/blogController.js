26db4786bf5b7b17d3c826ec02994d45ab0239ba
Shaurya <edshaurya@gmail.com>
2026-07-03 14:56:35 +0530

    implement CRUD functionality for blog API

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -12,4 +12,79 @@ const createBlog = async (req, res) => {
   res.status(201).json(blog);
 };
 
-module.exports = { createBlog };
+const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+
+    res.status(200).json(blogs);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+const getBlogById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const blog = await Blog.findById(id);
+
+    if (!blog) {
+      res.status(404).json({
+        message: "Blog not found",
+      });
+    }
+
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+const updateBlog = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    if (!updatedBlog) {
+      return res.status(404).josn({
+        message: "Blog Not Found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+const deleteBlog = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteBlog = await Blog.findByIdAndDelete(id);
+
+    if (!deleteBlog) {
+      return res.status(404).josn({
+        message: "Blog Not Found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+module.exports = {
+  createBlog,
+  getAllBlogs,
+  getBlogById,
+  updateBlog,
+  deleteBlog,
+};
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const { createBlog } = require("../controllers/blogController");
+const {
+  createBlog,
+  getAllBlogs,
+  getBlogById,
+  updateBlog,
+  deleteBlog,
+} = require("../controllers/blogController");
 
 router.post("/blogs", createBlog);
+
+router.get("/blogs", getAllBlogs);
+
+router.get("/blogs/:id", getBlogById);
+
+router.put("/blogs/:id", updateBlog);
+
+router.delete("/blogs/:id", deleteBlog);
 
 module.exports = router;
